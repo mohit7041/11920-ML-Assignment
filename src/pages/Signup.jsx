beb6459687cb0e9a8e549ab7096f4ed7cd3c43c9
Kuldeep Solanki <kuldeepsolanki1854@gmail.com>
2023-02-09 20:02:03 +0530

    update signup page

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,21 +1,21 @@
-import React from "react";
-import Footer from "../components/Footer";
-import Navbar from "../components/Navbar";
-import { Link } from "react-router-dom";
+import React from 'react';
+import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
+import { Link } from 'react-router-dom';
 
 const Signup = () => {
   return (
     <>
       <Navbar />
-      <section className="landingmainsection">
+      {/* <section className="landingmainsection">
         <div style={{ flex: "1", margin: "55px" }}>
-          {/* <p
+          <p
             className={`text-3xl font-extrabold ${
               success ? 'text-green-600' : 'text-red-600'
             }`}
           >
             {message}
-          </p> */}
+          </p>
           <h2 className="mt-5 mb-5 text-4xl text-purple-600">
             Create an account
           </h2>
@@ -93,6 +93,90 @@ const Signup = () => {
               </span>
             </button>
           </form>
+        </div>
+      </section> */}
+
+      <section className="text-gray-600 body-font flex justify-center items-center w-1/2 mx-auto mt-5 h-[90vh]">
+        <div className=" bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
+          <h2 className="text-gray-900 text-2xl title-font mb-5">
+            Create an account
+          </h2>
+          <div className="relative mb-4">
+            <label
+              htmlFor="full-name"
+              className="leading-7 text-sm text-gray-600"
+            >
+              Full Name*
+            </label>
+            <input
+              type="text"
+              id="full-name"
+              name="full-name"
+              className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              data-ddg-inputtype="identities.fullName"
+              required
+            />
+          </div>
+          <div className="relative mb-4">
+            <label htmlFor="email" className="leading-7 text-sm text-gray-600">
+              Email*
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              data-ddg-inputtype="identities.emailAddress"
+              data-ddg-autofill="true"
+              required
+            />
+          </div>
+          <div className="relative mb-4">
+            <label htmlFor="email" className="leading-7 text-sm text-gray-600">
+              Password*
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              data-ddg-inputtype="identities.emailAddress"
+              data-ddg-autofill="true"
+              required
+            />
+          </div>
+          <div className="relative mb-4">
+            <label htmlFor="email" className="leading-7 text-sm text-gray-600">
+              SSC Marks
+            </label>
+            <input
+              type="text"
+              id="sscmarks"
+              name="sscmarks"
+              className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              data-ddg-inputtype="identities.emailAddress"
+              data-ddg-autofill="true"
+            />
+          </div>
+          <div className="relative mb-4">
+            <label htmlFor="email" className="leading-7 text-sm text-gray-600">
+              HSC Marks
+            </label>
+            <input
+              type="text"
+              id="hscmarks"
+              name="hscmarks"
+              className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              data-ddg-inputtype="identities.emailAddress"
+              data-ddg-autofill="true"
+            />
+          </div>
+          <button
+            type="submit"
+            className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+          >
+            Signup
+          </button>
         </div>
       </section>
       <Footer />
