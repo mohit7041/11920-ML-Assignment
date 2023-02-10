88000257ec6b183cc152568587f439385780569f
Kuldeep Solanki <kuldeepsolanki1854@gmail.com>
2023-02-10 12:36:30 +0530

    set up signin and signup

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,61 +1,95 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import { Link } from 'react-router-dom';
 
 const Signin = () => {
+  const initialState = {
+    email: '',
+    password: '',
+    error: '',
+    loading: false,
+    didRedirect: false,
+  };
+
+  const [formValues, setFormValues] = useState(initialState);
+  const { email, password, error, loading, didRedirect } = formValues;
+
+  const handleSignin = (e) => {
+    e.preventDefault();
+    console.log(formValues);
+  };
+
+  const handleChange = (field) => (e) => {
+    setFormValues({ ...formValues, error: false, [field]: e.target.value });
+  };
   return (
     <>
       <Navbar />
       <section className="text-gray-600 body-font flex justify-center items-center w-1/2 mx-auto mt-5 h-[90vh]">
-        <div className=" bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
-          <h2 className="text-gray-900 text-2xl title-font mb-2">
-            Sign in to your account
-          </h2>
+        <form
+          className=" bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0"
+          onSubmit={handleSignin}
+        >
+          <div className=" bg-gray-100 rounded-lg  flex flex-col md:ml-auto w-full md:mt-0">
+            <h2 className="text-gray-900 text-2xl title-font mb-2">
+              Sign in to your account
+            </h2>
 
-          <p className="my-2">
-            New user?{' '}
-            <span className="text-purple-500 cursor-pointer font-bold">
-              <Link to="/signup">Signup</Link>
-            </span>
-          </p>
+            <p className="my-2">
+              New user?{' '}
+              <span className="text-purple-500 cursor-pointer font-bold">
+                <Link to="/signup">Signup</Link>
+              </span>
+            </p>
 
-          <div className="relative mb-4">
-            <label htmlFor="email" className="leading-7 text-sm text-gray-600">
-              Email*
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              data-ddg-inputtype="identities.emailAddress"
-              data-ddg-autofill="true"
-              required
-            />
+            <div className="relative mb-4">
+              <label
+                htmlFor="email"
+                className="leading-7 text-sm text-gray-600"
+              >
+                Email*
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                data-ddg-inputtype="identities.emailAddress"
+                data-ddg-autofill="true"
+                required
+                value={email}
+                onChange={handleChange('email')}
+              />
+            </div>
+            <div className="relative mb-4">
+              <label
+                htmlFor="email"
+                className="leading-7 text-sm text-gray-600"
+              >
+                Password*
+              </label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                data-ddg-inputtype="identities.emailAddress"
+                data-ddg-autofill="true"
+                required
+                value={password}
+                onChange={handleChange('password')}
+              />
+            </div>
+
+            <button
+              type="submit"
+              className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+            >
+              Signin
+            </button>
           </div>
-          <div className="relative mb-4">
-            <label htmlFor="email" className="leading-7 text-sm text-gray-600">
-              Password*
-            </label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              data-ddg-inputtype="identities.emailAddress"
-              data-ddg-autofill="true"
-              required
-            />
-          </div>
-
-          <button
-            type="submit"
-            className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
-          >
-            Signin
-          </button>
-        </div>
+        </form>
       </section>
       <Footer />
     </>
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -34,110 +34,128 @@ const Signup = () => {
     // convert ssc and hsc marks to numbers
     const sscMarksNum = Number(sscMarks);
     const hscMarksNum = Number(hscMarks);
+
+    console.log(formValues);
   };
   return (
     <>
       <Navbar />
 
       <section className="text-gray-600 body-font flex justify-center items-center w-1/2 mx-auto mt-5 h-[90vh]">
-        <div className=" bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
-          <h2 className="text-gray-900 text-2xl title-font mb-5">
-            Create an account
-          </h2>
-          <p className="my-2">
-            Already have an account?{' '}
-            <span className="text-purple-500 cursor-pointer font-bold">
-              <Link to="/signin">Signin</Link>
-            </span>
-          </p>
-          <div className="relative mb-4">
-            <label
-              htmlFor="full-name"
-              className="leading-7 text-sm text-gray-600"
+        <form
+          className="bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0"
+          onSubmit={handleSignup}
+        >
+          <div className=" bg-gray-100 rounded-lg flex flex-col md:ml-auto w-full  md:mt-0">
+            <h2 className="text-gray-900 text-2xl title-font mb-5">
+              Create an account
+            </h2>
+            <p className="my-2">
+              Already have an account?{' '}
+              <span className="text-purple-500 cursor-pointer font-bold">
+                <Link to="/signin">Signin</Link>
+              </span>
+            </p>
+            <div className="relative mb-4">
+              <label
+                htmlFor="full-name"
+                className="leading-7 text-sm text-gray-600"
+              >
+                Full Name*
+              </label>
+              <input
+                type="text"
+                id="full-name"
+                name="full-name"
+                className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                data-ddg-inputtype="identities.fullName"
+                required
+                value={fullName}
+                onChange={handleChange('fullName')}
+              />
+            </div>
+            <div className="relative mb-4">
+              <label
+                htmlFor="email"
+                className="leading-7 text-sm text-gray-600"
+              >
+                Email*
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                data-ddg-inputtype="identities.emailAddress"
+                data-ddg-autofill="true"
+                required
+                value={email}
+                onChange={handleChange('email')}
+              />
+            </div>
+            <div className="relative mb-4">
+              <label
+                htmlFor="email"
+                className="leading-7 text-sm text-gray-600"
+              >
+                Password*
+              </label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                data-ddg-inputtype="identities.emailAddress"
+                data-ddg-autofill="true"
+                required
+                value={password}
+                onChange={handleChange('password')}
+              />
+            </div>
+            <div className="relative mb-4">
+              <label
+                htmlFor="email"
+                className="leading-7 text-sm text-gray-600"
+              >
+                SSC Marks
+              </label>
+              <input
+                type="text"
+                id="sscmarks"
+                name="sscmarks"
+                className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                data-ddg-inputtype="identities.emailAddress"
+                data-ddg-autofill="true"
+                value={sscMarks}
+                onChange={handleChange('sscMarks')}
+              />
+            </div>
+            <div className="relative mb-4">
+              <label
+                htmlFor="email"
+                className="leading-7 text-sm text-gray-600"
+              >
+                HSC Marks
+              </label>
+              <input
+                type="text"
+                id="hscmarks"
+                name="hscmarks"
+                className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                data-ddg-inputtype="identities.emailAddress"
+                data-ddg-autofill="true"
+                value={hscMarks}
+                onChange={handleChange('hscMarks')}
+              />
+            </div>
+            <button
+              type="submit"
+              className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
             >
-              Full Name*
-            </label>
-            <input
-              type="text"
-              id="full-name"
-              name="full-name"
-              className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              data-ddg-inputtype="identities.fullName"
-              required
-              value={fullName}
-              onChange={handleChange('fullName')}
-            />
+              Signup
+            </button>
           </div>
-          <div className="relative mb-4">
-            <label htmlFor="email" className="leading-7 text-sm text-gray-600">
-              Email*
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              data-ddg-inputtype="identities.emailAddress"
-              data-ddg-autofill="true"
-              required
-              value={email}
-              onChange={handleChange('email')}
-            />
-          </div>
-          <div className="relative mb-4">
-            <label htmlFor="email" className="leading-7 text-sm text-gray-600">
-              Password*
-            </label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              data-ddg-inputtype="identities.emailAddress"
-              data-ddg-autofill="true"
-              required
-              value={password}
-              onChange={handleChange('password')}
-            />
-          </div>
-          <div className="relative mb-4">
-            <label htmlFor="email" className="leading-7 text-sm text-gray-600">
-              SSC Marks
-            </label>
-            <input
-              type="text"
-              id="sscmarks"
-              name="sscmarks"
-              className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              data-ddg-inputtype="identities.emailAddress"
-              data-ddg-autofill="true"
-              value={sscMarks}
-              onChange={handleChange('sscMarks')}
-            />
-          </div>
-          <div className="relative mb-4">
-            <label htmlFor="email" className="leading-7 text-sm text-gray-600">
-              HSC Marks
-            </label>
-            <input
-              type="text"
-              id="hscmarks"
-              name="hscmarks"
-              className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              data-ddg-inputtype="identities.emailAddress"
-              data-ddg-autofill="true"
-              value={hscMarks}
-              onChange={handleChange('hscMarks')}
-            />
-          </div>
-          <button
-            type="submit"
-            onClick={handleSignup}
-            className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
-          >
-            Signup
-          </button>
-        </div>
+        </form>
       </section>
       <Footer />
     </>
