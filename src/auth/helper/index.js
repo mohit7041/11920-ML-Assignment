19929943a83d3f035f3898dbb87d8e4b91a2a2ac
Kuldeep Solanki <kuldeepsolanki1854@gmail.com>
2023-02-10 16:08:28 +0530

    get logged in user in dashboard

--- src/auth/helper/index.js
+++ src/auth/helper/index.js
@@ -66,3 +66,15 @@ export const signout = (next) => {
       .catch((err) => console.log(err));
   }
 };
+
+//  get current user method
+export const getCurrentUser = () => {
+  if (typeof window == 'undefined') {
+    return false;
+  }
+  if (localStorage.getItem('jwt')) {
+    return JSON.parse(localStorage.getItem('jwt'));
+  } else {
+    return false;
+  }
+};
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getCurrentUser } from '../auth/helper';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const [user, setUser] = useState('');
+  useEffect(() => {
+    // get user info from local storage
+    const user = getCurrentUser().user;
+    setUser(user);
+  }, []);
+  return (
+    <>
+      <Navbar />
+      <section className="h-[90vh] px-4 py-6">
+        <div>
+          <h1 className="text-3xl text-center">Welcome to your Dashboard!</h1>
+        </div>
+      </section>
+      <Footer />
+    </>
+  );
 };
 
 export default Dashboard;
