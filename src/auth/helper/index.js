763d69cabb4daa6ea6f6eb363cbcacbb53013c94
Kuldeep Solanki <kuldeepsolanki1854@gmail.com>
2023-02-10 12:48:30 +0530

    add auth helpers

---
+++ src/auth/helper/index.js
@@ -0,0 +1,68 @@
+import { API } from '../../backend';
+
+// authenticate method
+export const authenticate = (data, next) => {
+  if (typeof window !== 'undefined') {
+    localStorage.setItem('jwt', JSON.stringify(data));
+    next();
+  }
+};
+
+// isAuthenticated method
+export const isAuthenticated = () => {
+  if (typeof window == 'undefined') {
+    return false;
+  }
+  if (localStorage.getItem('jwt')) {
+    return JSON.parse(localStorage.getItem('jwt'));
+  } else {
+    return false;
+  }
+};
+
+// user signup method
+export const signup = (user) => {
+  return fetch(`${API}/signup`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(user),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
+
+// user signin method
+export const signin = (user) => {
+  return fetch(`${API}/signin`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(user),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
+
+//  user signout method
+export const signout = (next) => {
+  if (typeof window !== 'undefined') {
+    localStorage.removeItem('jwt');
+    next();
+    return fetch(`${API}/signout`, {
+      method: 'GET',
+    })
+      .then((response) => {
+        console.log('signout', response);
+      })
+      .catch((err) => console.log(err));
+  }
+};
