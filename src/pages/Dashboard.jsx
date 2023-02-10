0c7186a95125be09ba4b8e6bc8cb8f00e60bd2f9
Kuldeep Solanki <kuldeepsolanki1854@gmail.com>
2023-02-10 19:03:52 +0530

    add button in dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -17,6 +17,17 @@ const Dashboard = () => {
       <section className="h-[90vh] px-4 py-6">
         <div>
           <h1 className="text-3xl text-center">Welcome to your Dashboard!</h1>
+          <div className="text-center my-5">
+            <a
+              href="#_"
+              class="px-5 py-2.5 relative rounded group overflow-hidden font-medium bg-purple-50 text-purple-600 inline-block"
+            >
+              <span class="absolute top-0 left-0 flex w-full h-0 mb-0 transition-all duration-200 ease-out transform translate-y-0 bg-purple-600 group-hover:h-full opacity-90"></span>
+              <span class="relative group-hover:text-white">
+                Take Assessment
+              </span>
+            </a>
+          </div>
         </div>
       </section>
       <Footer />
